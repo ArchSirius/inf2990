7854f35ebf6578963e961d5b7b68b8ef5ec073e8
Samuel Rondeau <samuel.rondeau@polymtl.ca>
2015-09-11 20:00:45 -0400

    Add DebugSelect window

---
+++ Sources/InterfaceGraphique/DebugSelect.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterfaceGraphique
+{
+    /// <summary>
+    /// Interaction logic for UserControl2.xaml
+    /// </summary>
+    /// Tiré de http://wpftutorial.net/Home.html
+    public partial class DebugSelect : Window
+    {
+        public DebugSelect()
+        {
+            InitializeComponent();
+        }
+    }
+}
