c8ff9cc075e8c0fb659c6b4540a0c93311b7d7b1
Louis Racicot <info@louisracicot.com>
2015-09-25 11:25:38 -0400

    add debug and rotation class

---
+++ Sources/InterfaceGraphique/Debug.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InterfaceGraphique
+{
+    class Debug
+    {
+        public static void Write(String msg)
+        {
+            if (IsDebugEnabled())
+            {
+                System.Console.WriteLine(msg);
+            }
+        }
+
+        public static bool IsDebugEnabled()
+        {
+            #if DEBUG
+            return true;
+            #else 
+            return false;
+            #endif
+        }
+    }
+}
---
+++ Sources/InterfaceGraphique/Tools/Scale.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfaceGraphique.Tools
+{
+    class Scale : Tool
+    {
+        public Scale(ToolContext context)
+            : base(context)
+        {
+
+        }
+
+        public override void LeftMouseClicked(MouseEventArgs e)
+        {
+
+        }
+
+        public override void LeftMouseReleased(MouseEventArgs e)
+        {
+
+        }
+
+        public override void Dragging(int deltaX, int deltaY, int deltaZ)
+        {
+            FonctionsNatives.scale();
+        }
+
+        static partial class FonctionsNatives
+        {
+            [DllImport(@"Noyau.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern void scale();
+        }
+    }
+}
