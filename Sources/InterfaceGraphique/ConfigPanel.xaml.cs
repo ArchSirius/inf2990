0d32d51438ce50272334d3e5c1b1652c03f4b936
Louis Racicot <info@louisracicot.com>
2015-10-26 15:41:08 -0400

    add add config panel

---
+++ Sources/InterfaceGraphique/ConfigPanel.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace InterfaceGraphique
+{
+    /// <summary>
+    /// Interaction logic for ConfigPanel.xaml
+    /// </summary>
+    public partial class ConfigPanel : Page, Renderable
+    {
+        public delegate void ClickEventHandler(object sender, EventArgs e);
+        public event ClickEventHandler LoadMainMenu;
+
+        public ConfigPanel()
+        {
+            InitializeComponent();
+        }
+
+        public void FrameUpdate(double time)
+        {
+            try
+            {
+                Action action = delegate()
+                {
+                    System.Threading.Thread.Sleep(1);
+                };
+
+                Dispatcher.Invoke(DispatcherPriority.Normal, action);
+            }
+            catch (Exception e)
+            {
+                Debug.Write("FrameUpdate error: " + e.GetBaseException().Message);
+            }
+        }
+    }
+}
