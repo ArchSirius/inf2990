27f9e53f88eb34cd747da3dc426122e34a899d37
Louis Racicot <info@louisracicot.com>
2015-10-30 12:42:29 -0400

    Config des touches du clavier

--- Sources/InterfaceGraphique/ConfigPanel.xaml.cs
+++ Sources/InterfaceGraphique/ConfigPanel.xaml.cs
@@ -23,6 +23,7 @@ namespace InterfaceGraphique
     {
         public delegate void ClickEventHandler(object sender, EventArgs e);
         public event ClickEventHandler LoadMainMenu;
+        private ConfigPanelController controller;
 
         public ConfigPanel()
         {
@@ -44,6 +45,48 @@ namespace InterfaceGraphique
             {
                 Debug.Write("FrameUpdate error: " + e.GetBaseException().Message);
             }
+        }
+
+        private void KeyBinding_KeyDown(object sender, KeyEventArgs e)
+        {
+            Keyboard.ClearFocus();
+
+            var i = VisualTreeHelper.GetChildrenCount(KeyboardConfigGroup);
+
+            while (--i >= 0)
+            {
+                var child = VisualTreeHelper.GetChild(KeyboardConfigGroup, i);
+
+                if (child is TextBox && child != sender && ((TextBox)child).Text == e.Key.ToString())
+                {
+                    System.Windows.MessageBox.Show("Cette touche est déjà utilisé pour une autre action", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                    e.Handled = true;
+                    return;
+                }
+            }
+
+            if (e.Key != Key.Escape)
+            {
+                var text = e.Key.ToString();
+
+                if (e.Key == Key.Space)
+                {
+                    text = "Space";
+                }
+
+                ((TextBox)sender).Text = text;
+            }
+
+            Keyboard.ClearFocus();
+        }
+
+        private void KeyboardDefault_Click(object sender, RoutedEventArgs e)
+        {
+            KeyAvancer.Text = "W";
+            KeyReculer.Text = "S";
+            KeyRotationH.Text = "A";
+            KeyRotationAH.Text = "D";
+            KeyManuel.Text = "Space";
         }
     }
 }
